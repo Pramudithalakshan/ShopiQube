320d1ff4bc41a817ef0e6483318cb652003fc792
Pramudithalakshan <pramudithalakshan00@gmail.com>
2025-12-15 19:58:45 +0530

    Add getMainMenuOption() and switch case

--- main.cpp
+++ main.cpp
@@ -1,13 +1,49 @@
 #include <iostream>
 #include <cstdio>
 using namespace std;
-int main(){
-cout<<"-----------------------------------------------------"<< endl;
-cout<<"|                     ShopiQube                     |"<< endl;
-cout<<"-----------------------------------------------------" << endl;
-cout<< endl;
-cout<< endl;
-printf("%s\n%s\n%s\n%s\n%s\n%s\n\n","[1] place Order","[2] Search Order","[3] Search Customer","[4] Search Best Customer","[5] Update Customer","[6] Exit");
-cout<<"Enter option to continue"<<endl;
-return 0;
+
+bool getMainMenuOption(int option)
+{
+    switch (option)
+    {
+    case 1:
+        cout<<"This is option 1"<<endl;
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    case 6:
+        break;
+    default:
+        cout << "Ente valid number";
+        return false;
+    }
+    return true;
+}
+
+int main()
+{
+
+    while (true)
+    {
+        cout << "-----------------------------------------------------" << endl;
+        cout << "|                     ShopiQube                     |" << endl;
+        cout << "-----------------------------------------------------" << endl;
+        cout << endl;
+        cout << endl;
+        printf("%s\n%s\n%s\n%s\n%s\n%s\n\n", "[1] place Order", "[2] Search Order", "[3] Search Customer", "[4] Search Best Customer", "[5] Update Customer", "[6] Exit");
+        cout << "Enter option to continue";
+        int option;
+        cin >> option;
+        if (!getMainMenuOption(option))
+        {
+            continue;
+        }
+        return 0;
+    }
 }
