c51453ea8432510f29572f34dbfe5cbeaa4847d1
Pramudithalakshan <pramudithalakshan00@gmail.com>
2025-12-15 19:39:23 +0530

    Feature : Add main menu

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 #include <cstdio>
 using namespace std;
 int main(){
-
+cout<<"-----------------------------------------------------"<< endl;
+cout<<"|                     ShopiQube                     |"<< endl;
+cout<<"-----------------------------------------------------" << endl;
+cout<< endl;
+cout<< endl;
+printf("%s\n%s\n%s\n%s\n%s\n%s\n\n","[1] place Order","[2] Search Order","[3] Search Customer","[4] Search Best Customer","[5] Update Customer","[6] Exit");
+cout<<"Enter option to continue"<<endl;
 return 0;
 }
